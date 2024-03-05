5ccc1fb2475b2d7d0480602b990bb64e222fd387
Nomena ANDRIAMAHAFALY <nomenaandriamahafaly@gmail.com>
2024-03-05 22:05:07 +0300

    Initial commit

---
+++ src/test/java/com/example/GesPat/GesPatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GesPat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GesPatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
